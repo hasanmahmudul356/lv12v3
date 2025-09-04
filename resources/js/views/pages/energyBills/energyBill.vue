3caff83ce4b2fd68c155e386c8724deffebbc9b2
mdshihabuddin78 <shihab110897@gmail.com>
2025-09-04 17:20:02 +0600

    energyBill

---
+++ resources/js/views/pages/energyBills/energyBill.vue
@@ -0,0 +1,97 @@
+
+<script setup>
+    import {dataTable,fromModal,tableTop } from '@/components';
+
+    import {ref, onMounted} from 'vue';
+    import {useStore} from 'vuex';
+    const store = useStore();
+    import {useBase, useHttp, appStore} from '@/lib';
+
+    const {getDependency, submitForm, editData, deleteRecord} = {...useHttp()};
+    const {formFilter, formObject, openModal, closeModal, useGetters, dataList,changeStatus,statusBadge, httpRequest, pageDependencies, updateId} = {
+        ...useBase(),
+        ...appStore(),
+        ...useHttp(),
+        ...appStore().useGetters('dataList', 'httpRequest', 'pageDependencies', 'updateId')
+    };
+
+    const tableHeaders = ref(["#", "Energy Type","Bill Month", "Unite", "Unite Rate", "Customer Unite", "Status", "Actions"]);
+    const {getDataList, httpReq} = useHttp();
+
+    onMounted(() => {
+        getDataList();
+    });
+</script>
+<template>
+    <dataTable :headings="tableHeaders" :setting="true">
+        <template v-slot:tableTop >
+            <tableTop :defaultObject="{type:''}"></tableTop>
+        </template>
+        <template v-slot:data>
+            <tr v-for="(item, index) in dataList.data" :key="item.id">
+                <td>{{index+1}}</td>
+                <td>{{ item.type == 1 ? 'Generator' : item.type == 2 ? 'Solar' : '' }}</td>
+                <td>{{ item.billing_month}}</td>
+                <td>{{ item.unit }}</td>
+                <td>{{ item.unit_rate }}</td>
+                <td>{{ item.customer_unit }}</td>
+                <td><a @click="changeStatus({obj:item})" class="pointer" v-html="statusBadge(item.status)"></a></td>
+                <td>
+                    <a @click="editData({data:item, id:item.id, modal:'fromModal'})" class="btn btn-outline-secondary action">
+                        <i class='bx bxs-edit text-warning'></i>
+                    </a>
+                    <a @click="deleteRecord({targetId:item.id,listIndex:index, listObject:dataList.data})" class="btn btn-outline-secondary action">
+                        <i class='bx bxs-trash text-danger'></i>
+                    </a>
+                </td>
+            </tr>
+        </template>
+
+        <fromModal @submit="submitForm({
+            modal: 'fromModal',
+            callback: function (retData) {
+                Object.assign(formObject, {});
+                getDataList();
+            }
+        })">
+            <div class="row mb-2">
+                <label class="col-md-4"><strong>Energy Type: </strong></label>
+                <div class="col-md-8">
+                    <select v-model="formObject.type" class="form-control" v-validate="'required'">
+                        <option value="">Select</option>
+                        <option value="1">Generator</option>
+                        <option value="2">Solar</option>
+
+                    </select>
+                </div>
+            </div>
+
+            <div class="row mb-2">
+                <label class="col-md-4"><strong>Billing Month : </strong></label>
+                <div class="col-md-8">
+                    <input type="month" v-validate="'required'" v-model="formObject.billing_month" class="form-control"/>
+                </div>
+            </div>
+            <div class="row mb-2">
+                <label class="col-md-4"><strong>Unit(kwh): </strong></label>
+                <div class="col-md-8">
+                    <input type="number" v-model="formObject.unit" v-validate="'required'" step="0.01" class="form-control"/>
+                </div>
+            </div>
+            <div class="row mb-2">
+                <label class="col-md-4"><strong>Unit Rate(Per kwh): </strong></label>
+                <div class="col-md-8">
+                    <input type="number" v-model="formObject.unit_rate" v-validate="'required'" step="0.01"  class="form-control"/>
+                </div>
+            </div>
+            <div class="row mb-2">
+                <label class="col-md-4"><strong>Customer Unit(Per kwh): </strong></label>
+                <div class="col-md-8">
+                    <input type="number" v-model="formObject.customer_unit" v-validate="'required'" step="0.01"  class="form-control"/>
+                </div>
+            </div>
+
+        </fromModal>
+    </dataTable>
+
+</template>
