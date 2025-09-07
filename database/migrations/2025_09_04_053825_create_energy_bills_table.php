28a3d46ceab4a5a7c98534e6497912042cece25a
mdshihabuddin78 <shihab110897@gmail.com>
2025-09-07 15:58:40 +0600

    energy bill

--- database/migrations/2025_09_04_053825_create_energy_bills_table.php
+++ database/migrations/2025_09_04_053825_create_energy_bills_table.php
@@ -13,12 +13,13 @@ return new class extends Migration
     {
         Schema::create('energy_bills', function (Blueprint $table) {
             $table->id();
-            $table->integer('type')->comment('1=generator, 2=solar');
+            $table->integer('type')->comment('1=generator, 2=solar , 3=nesco');
             $table->integer('user_id');
             $table->string('billing_month', 7)->comment('Format: YYYY-MM');
             $table->decimal('unit');
             $table->decimal('unit_rate')->comment('Per KWh');
             $table->decimal('customer_unit')->comment('Per KWh');
+            $table->integer('totalCustomers');
             $table->tinyInteger('status')->default(1)->comment('1=active,0=inactive');
             $table->timestamps();
         });
--- resources/js/views/pages/energyBills/energyBill.vue
+++ resources/js/views/pages/energyBills/energyBill.vue
@@ -8,7 +8,7 @@
     import {useBase, useHttp, appStore} from '@/lib';
 
     const {getDependency, submitForm, editData, deleteRecord} = {...useHttp()};
-    const {formFilter, formObject, openModal, closeModal, useGetters, dataList,changeStatus,statusBadge, httpRequest, pageDependencies, updateId} = {
+    const {formFilter, formObject, openModal, closeModal, useGetters,httpReq, dataList,changeStatus,statusBadge, httpRequest, pageDependencies, updateId} = {
         ...useBase(),
         ...appStore(),
         ...useHttp(),
@@ -16,11 +16,25 @@
     };
 
     const tableHeaders = ref(["#", "Energy Type","Bill Month", "Unite", "Unite Rate", "Customer Unite", "Status", "Actions"]);
-    const {getDataList, httpReq} = useHttp();
+    const {getDataList, } = useHttp();
 
     onMounted(() => {
         getDataList();
     });
+    const calculate = async ()=>{
+        const response = await httpReq({
+            url: '/customerKwh',
+            method: 'get',
+            params: { type: formObject.value.type, unit: formObject.value.unit }
+        });
+        if (response )
+        {
+            formObject.value.customer_unit = response.customerUnit;
+            formObject.value.totalCustomers = response.totalCustomers;
+        }
+
+    }
+
 </script>
 <template>
     <dataTable :headings="tableHeaders" :setting="true">
@@ -30,7 +44,7 @@
         <template v-slot:data>
             <tr v-for="(item, index) in dataList.data" :key="item.id">
                 <td>{{index+1}}</td>
-                <td>{{ item.type == 1 ? 'Generator' : item.type == 2 ? 'Solar' : '' }}</td>
+                <td>{{ item.type === 1 ? 'Generator' : item.type === 2 ? 'Solar' : item.type ===3 ? 'Nesco' : '' }}</td>
                 <td>{{ item.billing_month}}</td>
                 <td>{{ item.unit }}</td>
                 <td>{{ item.unit_rate }}</td>
@@ -61,11 +75,10 @@
                         <option value="">Select</option>
                         <option value="1">Generator</option>
                         <option value="2">Solar</option>
-
+                        <option value="3">Nesco</option>
                     </select>
                 </div>
             </div>
-
             <div class="row mb-2">
                 <label class="col-md-4"><strong>Billing Month : </strong></label>
                 <div class="col-md-8">
@@ -74,24 +87,34 @@
             </div>
             <div class="row mb-2">
                 <label class="col-md-4"><strong>Unit(kwh): </strong></label>
-                <div class="col-md-8">
+                <div class="col-md-7">
                     <input type="number" v-model="formObject.unit" v-validate="'required'" step="0.01" class="form-control"/>
                 </div>
-            </div>
-            <div class="row mb-2">
-                <label class="col-md-4"><strong>Unit Rate(Per kwh): </strong></label>
-                <div class="col-md-8">
-                    <input type="number" v-model="formObject.unit_rate" v-validate="'required'" step="0.01"  class="form-control"/>
+                <div class="col-md-1 font-24" @click="calculate">
+                    <i class="bx bx-calculator"></i>
                 </div>
             </div>
             <div class="row mb-2">
-                <label class="col-md-4"><strong>Customer Unit(Per kwh): </strong></label>
+                <label class="col-md-4"><strong>Total Customer: </strong></label>
                 <div class="col-md-8">
-                    <input type="number" v-model="formObject.customer_unit" v-validate="'required'" step="0.01"  class="form-control"/>
+                    <input type="number" v-model="formObject.totalCustomers" class="form-control" readonly/>
+                </div>
+            </div>
+            <div class="row mb-2">
+                <label class="col-md-4"><strong>Customer Unit (Per kwh): </strong></label>
+                <div class="col-md-8">
+                    <input type="text" v-model="formObject.customer_unit" class="form-control" readonly/>
                 </div>
             </div>
 
+            <div class="row mb-2">
+                <label class="col-md-4"><strong>Unit Rate(Per kwh): </strong></label>
+                <div class="col-md-8">
+                    <input type="number" v-model="formObject.unit_rate" step="0.01" class="form-control"/>
+                </div>
+            </div>
         </fromModal>
+
     </dataTable>
 
 </template>
