14dee2b0edcbd879933378c5f89647345c1b9061
mdshihabuddin78 <shihab110897@gmail.com>
2025-09-08 17:49:29 +0600

    customer notification

--- database/migrations/2025_09_03_145916_create_notifications_table.php
+++ database/migrations/2025_09_03_145916_create_notifications_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
-            $table->integer('type');
-            $table->text('message')->nullable();
-            $table->string('link')->nullable();
-            $table->integer('modal')->default(0);
+            $table->integer('customer_id');
+            $table->integer('user_id');
+            $table->string('title')->nullable();
+            $table->text('message');
+            $table->enum('delivery_method', ['email', 'sms']);
+            $table->boolean('is_read')->default(false);
+            $table->tinyInteger('status')->default(1)->comment('1=active,0=inactive');
             $table->timestamps();
         });
     }
